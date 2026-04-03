861536bcd5535ed4db7821ba3c7eab964e8ffebd
Chris Williams <chris@chrispwill.com>
2026-04-03 20:13:47 +0900

    Test out doubly linked list

--- tests/example_test.cpp
+++ tests/example_test.cpp
@@ -1,13 +1,78 @@
 #include <doctest/doctest.h>
+#include <memory>
 
-int factorial(int number) {
-  return number > 1 ? factorial(number - 1) * number : 1;
-}
+struct DLinkedNode {
+  std::shared_ptr<DLinkedNode> next;
+  std::weak_ptr<DLinkedNode> prev;
+  int data;
 
-TEST_CASE("testing the factorial function") {
-  CHECK(factorial(0) == 1);
-  CHECK(factorial(1) == 1);
-  CHECK(factorial(2) == 2);
-  CHECK(factorial(3) == 6);
-  CHECK(factorial(10) == 3628800);
+  explicit DLinkedNode(int val) : data(val) {}
+};
+
+class DLinkedIterator {
+private:
+  std::shared_ptr<DLinkedNode> current;
+
+public:
+  int get() { return current->data; }
+
+  // TODO: Improve signature of this for chaining
+  bool next() {
+    if (current->next == nullptr) {
+      return false;
+    }
+
+    current = current->next;
+    return true;
+  }
+
+  bool prev() {
+    if (current->prev.lock() == nullptr) {
+      return false;
+    }
+
+    current = current->next;
+    return true;
+  }
+
+  explicit DLinkedIterator(std::shared_ptr<DLinkedNode> current) {
+    this->current = current;
+  };
+};
+
+class DLinkedList {
+private:
+  std::shared_ptr<DLinkedNode> head;
+  std::shared_ptr<DLinkedNode> tail;
+
+public:
+  DLinkedIterator startIterator() { return DLinkedIterator(head); }
+  DLinkedIterator endIterator() { return DLinkedIterator(tail); }
+
+  void insertEnd(int val) {
+    auto newNode = std::make_shared<DLinkedNode>(val);
+
+    if (!head) {
+      head = tail = newNode;
+      return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+  }
+};
+
+TEST_CASE("testing the doubly linked list") {
+  auto list = std::make_shared<DLinkedList>();
+  list->insertEnd(123);
+  list->insertEnd(345);
+  list->insertEnd(678);
+
+  auto it = list->startIterator();
+  auto hasNext = it.next();
+  CHECK(hasNext);
+  CHECK(it.get() == 345);
+  hasNext = it.next();
+  CHECK(hasNext);
+  CHECK(it.get() == 678);
 }
